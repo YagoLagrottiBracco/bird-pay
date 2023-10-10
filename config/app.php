43162fb33c9e3ea6ec1bb50333ec61f186b25e4a
Yago Lagrotti Bracco <yago.lagrotti@outlook.com>
2023-10-10 20:44:19 -0300

    change url application on stage

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://dev.pixelprime.tech/bird-pay'),
 
     'asset_url' => env('ASSET_URL', null),
 
