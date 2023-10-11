fbfd3907fa88f11dfbbf48b29ac6df469fda087f
Yago Lagrotti Bracco <yago.lagrotti@outlook.com>
2023-10-11 19:12:42 -0300

    some changes on the code

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import { useDarkModeStore } from './stores/darkMode.js'
 import { darkModeKey } from './config.js'
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Bird Pay'
@@ -12,7 +13,7 @@ const pinia = createPinia()
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
-  resolve: (name) => import(`./Pages/${name}.vue`),
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     return createApp({ render: () => h(App, props) })
       .use(plugin)
