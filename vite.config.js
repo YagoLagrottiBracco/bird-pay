213b81b0d9f0d150295cc0439e672f8aba9ee96d
Yago Lagrotti Bracco <yago.lagrotti@outlook.com>
2023-10-11 18:16:53 -0300

    some changes on the code

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         vue({
             template: {
                 transformAssetUrls: {
-                    base: null,
+                    base: '/bird-pay',
                     includeAbsolute: false,
                 },
             },
