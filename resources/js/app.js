c113296007cae5c67e52a8755602351893ff1f14
Yago Lagrotti Bracco <yago.lagrotti@outlook.com>
2023-10-11 19:19:55 -0300

    some changes on the code

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,11 +9,12 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Bird Pay'
+const { glob } = import.meta
 const pinia = createPinia()
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
-  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     return createApp({ render: () => h(App, props) })
       .use(plugin)
