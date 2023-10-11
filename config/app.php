d2f35c6d6b16f482b55233e235e71c4c96eda980
Yago Lagrotti Bracco <yago.lagrotti@outlook.com>
2023-10-11 19:26:39 -0300

    some changes on the code

--- config/app.php
+++ config/app.php
@@ -54,9 +54,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://dev.pixelprime.tech/bird-pay/public'),
+    'url' => env('APP_URL', './'),
 
-    'asset_url' => env('ASSET_URL', 'https://dev.pixelprime.tech/bird-pay/public'),
+    'asset_url' => env('ASSET_URL', './'),
 
     /*
     |--------------------------------------------------------------------------
